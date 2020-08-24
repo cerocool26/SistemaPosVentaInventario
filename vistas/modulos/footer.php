3503af2dc4f9ca3894ba058586b429acff7209e2
cerocool26 <andrescamiloro26@gmail.com>
2020-08-24 18:14:03 -0500

    version 1

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2019<a href="" target="_blank">Andres Camilo Romero Rodriguez</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
